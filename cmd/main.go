fdc7ab8711b4dc0b75bc76364e574dcfe5446546
Ulugbek <abdurahmons100@gmail.com>
2021-05-21 22:17:18 +0500

    Last commit for today

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,40 @@
 package main
 
 import (
-	"github.com/Ulugbek999/http.git/pkg/server"
-	"log"
+	"github.com/Ulugbek999/http.git/cmd/app"
+	"github.com/Ulugbek999/http.git/pkg/banners"
 	"net"
+	"net/http"
 	"os"
+
+	
 )
 
 func main() {
+	// хост по которому обрашемся (роут )
 	host := "0.0.0.0"
-	port := "9999"
 
-	log.Println("Server is listening...")
+	// порт роута
+	port := "9999"
+	// визивае фнукцию запуска сервера
 	if err := execute(host, port); err != nil {
+		// если вернул ошибку остановис сервер
 		os.Exit(1)
 	}
 }
 
+// функция запск сервера
 func execute(host string, port string) (err error) {
-	srv := server.NewServer(net.JoinHostPort(host, port))
 
+	mux := http.NewServeMux()
+	bannersSvc := banners.NewService()
+	server := app.NewServer(mux, bannersSvc)
+	server.Init()
 
+	srv := http.Server{
+		Addr:    net.JoinHostPort(host, port),
+		Handler: server,
+	}
 
-
-	srv.Register("/category{id1}/{id2}", func(req *server.Request) {
-		id1 := req.PathParams["id1"]
-		log.Print(id1)
-		
-		id2 := req.PathParams["id2"]
-		log.Print(id2)
-
-		body := "About Golang Academy"
-		_, err = req.Conn.Write([]byte(srv.Response(body)))
-		if err != nil {
-			log.Print(err)
-			return
-		}
-	})
-
-	return srv.Start()
+	return srv.ListenAndServe()
 }
